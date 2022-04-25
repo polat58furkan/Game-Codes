3bba0839f7e984f1723bf3309e6a5e4e56fe2259
polat58furkan <94589795+polat58furkan@users.noreply.github.com>
2022-04-25 12:24:55 +0300

    Update BannerReklam.cs

--- BannerReklam.cs
+++ BannerReklam.cs
@@ -9,7 +9,7 @@ public class BannerReklam : MonoBehaviour
     private BannerView banner;
     void Start()
     {
-        banner = new BannerView("ca-app-pub-6760811376462562/9267220005",AdSize.Banner,AdPosition.Bottom);
+        banner = new BannerView("ca-app-pub-*************",AdSize.Banner,AdPosition.Bottom);
 
         AdRequest reklamistegi=new AdRequest.Builder().Build();
         banner.LoadAd(reklamistegi);
